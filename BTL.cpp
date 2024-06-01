7a4549fcfa15fd55a19a9cb28887978301b9aacc
PenTaGGk67 <ghhkdgf@gmail.com>
2024-06-02 01:49:00 +0700

    BTL

--- BTL.cpp
+++ BTL.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class SinhVien {
-private:
+private: 
     long mssv;
     char hoTen[100];
     char email[100];
@@ -38,6 +38,8 @@ public:
     const char* getMaHP() const { return MaHP; }
     const char* getTenHP() const { return TenHP; }
     int getTinchi() const { return Tinchi; }
+
+
 };
 
 class NodeSV {
@@ -67,6 +69,12 @@ public:
     NodeHP(const char* MaHP, const char* TenHP, int Tinchi)
     : data(MaHP, TenHP, Tinchi), next(nullptr) {}
 
+    const char* getMaHP() const {return data.getMaHP();}
+    const char* getTenHP() const {return data.getTenHP();}
+    int getTinchi() const {return data.getTinchi();}
+
+    NodeHP* getNext() const { return next; }
+    void setNext(NodeHP* nextNode) { next = nextNode; }
     HocPhan& getData() { return data; }
 };
 
@@ -88,6 +96,7 @@ public:
 };
 
 typedef NodeSV* LinkedList;
+typedef NodeHP* LinkedListHP;
 typedef NodeSV* PNodeSV;
 typedef NodeHP* PNodeHP;
 typedef NodeTKB* PNodeTKB;
@@ -103,7 +112,7 @@ int isEmpty(LinkedList H) {
 void insertBeginSV(LinkedList& H, long mssv, const char* hoTen, const char* email) {
     PNodeSV Q = new NodeSV(mssv, hoTen, email);
     if (isEmpty(H)) {
-        H = Q;
+        H = Q;   // Q -> next == NULL
     } else {
         Q->setNext(H);
         H = Q;
@@ -113,7 +122,7 @@ void insertBeginSV(LinkedList& H, long mssv, const char* hoTen, const char* emai
 void insertAfterSV(LinkedList& H, PNodeSV P, long mssv, const char* hoTen, const char* email) {
     PNodeSV Q = new NodeSV(mssv, hoTen, email);
     if (isEmpty(H)) {
-        H = Q;
+        H = Q; 
     } else {
         if (P != nullptr) {
             Q->setNext(P->getNext());
@@ -159,4 +168,78 @@ void DelectSV (LinkedList H, long mssv) {
     PNodeSV P = searchSV (H, mssv);
     if (P != NULL)
         delete P;
+}
+
+void InitListHP(LinkedListHP& H){
+    H = NULL;
+}
+bool isEmptyHocPhan(LinkedListHP H){
+    return ( H==NULL );
+}
+// Thêm 1 học phần vào trước danh sách
+void InsertHPBegin(LinkedListHP& H, const char* maHP, const char* tenHP, int tinchi){ 
+    PNodeHP Q = new NodeHP(maHP, tenHP, tinchi);
+    if(isEmptyHocPhan(H)){
+        Q->setNext(NULL);
+        H = Q;
+    } else {
+        Q->setNext(H);
+        H = Q;   
+    }
+}
+// Thêm 1 học phần vào sau 1 HP trỏ bởi P trong danh sách
+void InsertHPAfter(LinkedListHP& H, PNodeHP P, const char* maHP, const char* tenHP, int tinchi){
+    PNodeHP Q = new NodeHP(maHP, tenHP, tinchi);
+    if(isEmptyHocPhan(H)){
+        Q->setNext(NULL);
+        H = Q;
+    } else {
+        if(P == NULL){
+            cout << "Khong co hoc phan trong danh sach" << endl;
+            return;
+        }
+        else {
+            Q->setNext(P->getNext());
+            P->setNext(Q);
+        }
+    }
+}
+
+// Hiển thị ra màn hình các học phần
+void display(LinkedListHP H){
+    PNodeHP P = H;
+    while (P != NULL){
+        cout << P->getMaHP() << " " << P->getTenHP() << " " << P->getTinchi() << endl;
+        P = P->getNext();
+    }
+}
+// Tìm học phần có mã cho trước
+PNodeHP searchHP(LinkedListHP H, const char* MaHP){
+    PNodeHP P = H;
+    while (P != NULL){
+        if(strcmp(P->getData().getMaHP(), MaHP) == 0){ // So sánh
+            return P;
+        } else P = P->getNext();
+    }
+    return NULL;
+}
+
+// Xóa học phần có mã HP cho trước
+PNodeHP deleteHocPhan(LinkedListHP& H, const char* MaHP){
+    PNodeHP P = H;
+    PNodeHP R = NULL;
+    if(isEmptyHocPhan(H)){
+        cout << "Danh sach khong co gi de xoa\n";
+        return;
+    }
+    while (P != NULL &&  strcmp(P->getData().getMaHP(), MaHP) != 0){
+        R = P;
+        P = P ->getNext();
+    }
+    if (R == NULL){
+        H = P->getNext();
+    } else {
+        R->setNext(P->getNext());
+    }
+    delete P;
 }
