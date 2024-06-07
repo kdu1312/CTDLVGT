fdc0d731f12ea3a6721274cfc279b1c50a71884b
kdu1312 <168560837+kdu1312@users.noreply.github.com>
2024-06-08 00:09:22 +0700

    Update funtion.h

--- funtion.h
+++ funtion.h
@@ -1,5 +1,21 @@
+#pragma once
 #include "SinhVien.h"
 #include "TKB.h"
 #include "HocPhan.h"
 #include "dangnhap.h"
+
+void processMSSV(long mssv){
+    long mssvt = mssv;
+}
+;
+PNodeSV searchSV(LinkedListSV H, long mssvt) {
+    PNodeSV P = H;
+    while (P != NULL) {
+        if (P->data.mssv == mssvt)
+            return P;
+        else
+            P = P->next;
+    }
+    return NULL;
+}
 
