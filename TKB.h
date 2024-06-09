086f9728e239caeb5f0207c80be6475a4c11c6e9
PenTaGGk67 <ghhkdgf@gmail.com>
2024-06-10 00:27:54 +0700

    TKB

--- TKB.h
+++ TKB.h
@@ -16,3 +16,16 @@ struct NodeTKB {
 };
 
 typedef NodeTKB* PNodeTKB;
+// Thêm Học Phần vào Thời Khóa Biểu của 1 sinh viên
+void ThemHP(PNodeSV SV, const char* MaHP, const char* TenHP, int Tinchi, int Thu, int Start, int End, int Tuan, const char* PhongHoc, const char* Kihoc);
+
+// Tìm học phần trong TKB trả về con trỏ trỏ vào HP đó
+PNodeTKB searchTKB  (PNodeSV SV, const char* MaHP);
+
+// Xóa học phần trong tkb của Sinh Viên
+void DeleteTKB(PNodeSV& SV, const char* MaHP);
+
+//Hiển thị ra màn hình thời khóa biểu 
+void displayTKB(PNodeSV SV);
+
+
