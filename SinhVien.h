6157234964d325b6b96d0f36b12dae266fd077b6
GoSoLong <9atruong2@gmail.com>
2024-06-12 19:07:50 +0700

    SinhVien.h

--- SinhVien.h
+++ SinhVien.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "header.h"
 #include "HocPhan.h"
 #include "TKB.h"
 
@@ -22,14 +21,15 @@ void InitListSV(LinkedListSV& H); //khởi tạo danh sách sinh viên
 
 int isEmptySV(LinkedListSV H); //kiểm tra rỗng danh sách sinh viên
 
-void insertBeginSV(LinkedListSV& H, long mssvtarget, long mssv, const char* hoTen, const char* email); //Thêm 1 sinh viên vào đầu danh sách
+void insertBeginSV(LinkedListSV& H, SinhVien& sv); //Thêm 1 sinh viên vào đầu danh sách
 
-void insertAfterSV(LinkedListSV& H, long mssvtarget, long mssv, const char* hoTen, const char* email); //Thêm 1 sinh viên vào sau một sinh viên có mssv cho trước
+void insertAfterSV(LinkedListSV& H, long mssvtarget, SinhVien& sv); //Thêm 1 sinh viên vào sau một sinh viên có mssv cho trước
 
-void insertBeforeSV(LinkedListSV& H, long mssvtarget, long mssv, const char* hoTen, const char* email);//Thêm 1 sinh viên vào trước một sinh viên có mssv cho trước
+void insertBeforeSV(LinkedListSV& H, long mssvtarget, SinhVien& sv);//Thêm 1 sinh viên vào trước một sinh viên có mssv cho trước
 
 PNodeSV searchSV(LinkedListSV H, long mssv); //Tìm kiếm sinh viên có mssv cho trước
 
 void DelectSV (LinkedListSV H, long mssv); //Xóa sinh viên có mssv cho trước
 
 void displaySV(LinkedListSV H); //Hiển thị danh sách sinh viên
+
